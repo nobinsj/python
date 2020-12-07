b0466c36a841a293a93516a8c4d4b2603727b6a5
nobin <nobinsjohns@yahoo.com>
2020-12-07 19:16:08 +0530

    added comments

--- sample.py
+++ sample.py
@@ -1,5 +1,11 @@
 txt = "hello world - git check"
-print(txt)
+
+print(txt)  # sample print
+
 print("answer is : ", 2*5)
-print("\n")
-print(txt, end="")
+
+print("\n")  # newline
+
+print("first-sentance", end=" ")  # print space instead of newline
+
+print("second-sentance")
