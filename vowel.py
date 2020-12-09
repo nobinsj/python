3754ed7807402f793c4a090da2d54fd3e8a52c9f
nobin <nobinsjohns1@gmail.com>
2020-12-09 19:58:48 +0530

    vowel or not

---
+++ vowel.py
@@ -0,0 +1,8 @@
+# write  a program to check whether a given charcter is a vowel or not
+
+val = input(" Enter a Character : ")
+
+if(val == 'a' or val == 'e' or val == 'i' or val == 'o' or val == 'u'):
+    print("\n  Character you entered is Vowel ")
+else:
+    print("\n  Character you enterd is not vowel ")
